0843ac81e5e2f161fa3f76576cb2ce16814f1614
Grant Rigdon <grant_rigdon@ymail.com>
2018-10-11 19:37:29 -0400

    logic update

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -11,13 +11,16 @@ $(document).ready(function(){
     //   };
     //   firebase.initializeApp(config);
     // create wine list
+    $("#zoomin").hide();
     var wineList = [lightWhite = {cat:0 ,0:"Pinot Grigio", 1:"Savingion Blac"}, sweetWhite = {cat:1,0:"Moscato",1:"Riesling"}, richWhite = {cat:2,0:"Chardonnay",1:"Viognier"}, sparkling = {cat:3,0:"Champagne",1:"Prosecco"},rose = {cat:4,0:"Sweet Rose",1:"Dry Rose"},lightRed = {cat:5,0:"Pinot Noir",1:"Grenache/Garnacha"}, mediumRed={cat:6,0:"Merlot",1:"Zinfandel"}, boldRed = {cat:7,0:"Malbec",1:"Cabernet Sauvignon"}];
     // food options
         var foodList = [lightWhite = {0:"tuna",1:"trout",2:"kale",3:"chickpea"},sweetWhite = {0:"proscuitto",1:"habanero",2:"strawberry",3:"sweet+potato"}, richWhite = {0:"duck", 1:"lobster",2:"mushroom",3:"anise"},sparkling = {0:"oyster",1:"mussels",2:"clam",3:"pecorino"},rose={0:"turnip",1:"butternut",2:"pumpkin",3:"salami"},lightRed = {0:"chicken",1:"mushroom",2:"duck",3:"bacon"},mediumRed = {0:"pork",1:"gorgonzola",2:"eggplant",3:"tomato"},boldRed = {0:"beef",1:"lamb",2:"steak",3:"cheddar+cheese"}
     ];
     var type = ""
     var ingredient = ""
+    var zipCode = ""
     var foodUrl = "http://www.recipepuppy.com/api/?i="+ingredient
+    var wineUrl = "http://api.snooth.com/wines/?akey="+wineKey+"&q="+type+"&xp=40"
     // populate the dropdown menu
     for (i=0;i<wineList.length;i++){
         for (j=0;j<2;j++){
@@ -30,12 +33,13 @@ $(document).ready(function(){
     // when the button is clicked
     $(".wines").on("click",function(event){
         event.preventDefault();
-        type = this.id
-        console.log(this.type)
-        $("#selectwine").text(this.type);
+        type = this.type
+        console.log(type)
+        $("#selectwine").text(type);
         
         $("#search").on("click",function(){
             event.preventDefault();
+            $("#zoomin").show()
             for (i=0;i<4;i++){
                 ingredient = foodList[type][i];
                 console.log(ingredient);
@@ -46,20 +50,23 @@ $(document).ready(function(){
                 }).then(function(response){
                     // $("#f0").attr("src",response.results[0].href)
                     console.log(response.results[0].href)
-                }) 
-            }
-    
+                }); 
+            };
+            $.ajax({
+                url: wineUrl,
+                method: "GET"
+            }).then(function(response){
+                console.log(response)
+                $("#wine-table").append("<tr><th>Label</th><th>Wine</th><th>Vintage</th><th>Price</th><th>Link</th></tr>");
+                for (i=0;i<4;i++){
+                    $("#wine-table").append("<tr><td><img src='"+response.wines[i].image+"'></td><td>"+response.wines[i].name+"</td><td>"+response.wines[i].vintage+"</td><td>"+response.wines[i].price+"</td><td>"+response.wines[i].link+"</td></tr>");
+                }
+            });
 
-        })
+        });
     });
     
-    
+ 
 
-    $.ajax({
-        url: foodUrl,
-        method: "GET"
-    }).then(function(){
-
-    });
         
 });
